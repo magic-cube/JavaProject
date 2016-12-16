b7a5c6a7e8619616cbafbeedb939b426ad1129e6
magic-cube <15619225472@163.com>
2016-12-16 17:37:33 +0800

    生产者消费者问题

--- src/ProducerConsumer/ProducerConsumer.java
+++ src/ProducerConsumer/ProducerConsumer.java
@@ -7,10 +7,18 @@ package ProducerConsumer;
 public class ProducerConsumer {
 	public static void main(String args[]){
 		SynStack ss=new SynStack();
+		//使用同一个缓冲区
 		Producer p=new Producer(ss);
 		Consumer c=new Consumer(ss);
 		//一个生产者，一个消费者的情况
-		new Thread(p).start();
-		new Thread(c).start();
+		Thread p1=new Thread(p);
+		p1.start();
+		Thread c1=new Thread(c);
+		try{
+			c1.sleep(500);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		c1.start();
 	}
 }
