748e19bcf0f609503a2025c0f4d7cb0d5dbf6eb5
magic-cube <15619225472@163.com>
2016-12-20 00:42:42 +0800

    chat02

---
+++ src/chat02/ChatClient.java
@@ -0,0 +1,30 @@
+package chat02;
+import java.awt.*;
+import java.awt.event.*;
+
+public class ChatClient extends Frame{
+	
+	TextField tfTxt=new TextField();
+	TextArea taContent=new TextArea();
+	
+	public static void main(String args[]){
+		new ChatClient().launchFrame();
+	}
+	public void launchFrame(){
+		this.setLocation(400,300);
+		this.setSize(300, 300);
+		
+		add(tfTxt,BorderLayout.SOUTH);
+		add(taContent,BorderLayout.NORTH);
+		//关闭窗口
+		this.addWindowListener(new WindowAdapter(){
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+			
+		});
+		pack();//自动包装
+		
+		this.setVisible(true);
+	}
+}
